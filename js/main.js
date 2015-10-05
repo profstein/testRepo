d66476bcb8da87a4fe960d166247b68efe4a5a24
Christopher Stein <cstein@f1006-i-34171.bmcc.cuny.edu>
2015-10-05 17:09:21 -0400

    added ability to draw on the screeen and new blue color

--- js/main.js
+++ js/main.js
@@ -1,9 +1,12 @@
-var red;
+var red, blue;
 
 function setup(){
     red = color(255,0,0);
+    blue = color(0,0,255);
 }
 
 function draw(){
-
+    if(mouseIsPressed){
+     line(pmouseX,pmouseY,mouseX,mouseY);
+    }
 }
